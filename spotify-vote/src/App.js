5b19fb9cec0204ebc8ef788c2868458a8a3a97f0
codeahl <142953446+codeahl@users.noreply.github.com>
2024-08-09 19:23:52 -0500

    fixed stuff

--- spotify-vote/src/App.js
+++ spotify-vote/src/App.js
@@ -1,66 +1,71 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from "react";
 import './App.css';
 import axios from 'axios';
 
 function App() {
-  const CLIENT_ID = "dbf88f9cef4f4ce6ab5486db9b0400ba"
-  const REDIRECT_URI = "http://localhost:3000"
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-  const RESPONSE_TYPE = "token"
+    const CLIENT_ID = "b39c9c2f4fa346a69e4cdbcafefd5185"
+    const REDIRECT_URI = "http://localhost:3001"
+    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+    const RESPONSE_TYPE = "token"
 
-const [token, setToken] = useState("")
-const [searchKey, setSearchKey] = useState("")
-const [artists, setArtists] = useState([])
+    const [token, setToken] = useState("")
+    const [searchKey, setSearchKey] = useState("")
+    const [artists, setArtists] = useState([])
 
-useEffect(() => {
-  const hash = window.location.hash
-  let token = window.localStorage.getItem("token")
+    // const getToken = () => {
+    //     let urlParams = new URLSearchParams(window.location.hash.replace("#","?"));
+    //     let token = urlParams.get('access_token');
+    // }
 
-  if(!token && hash) {
-    token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+    useEffect(() => {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
 
-    window.localStorage.setItem("token", token)
-  }
+        // getToken()
 
-  setToken(token)
 
-})
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
 
-const logout = () => {
-  setToken("")
-  window.localStorage.removeItem("token")
-}
-
-  const searchArtists = async (e) => {
-    e.preventDefault()
-    const {data} = await axios.get("https://api.spotify.com/v1/search", {
-        headers: {
-            Authorization: `Bearer ${token}`
-        },
-        params: {
-            q: searchKey,
-            type: "artist"
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
         }
-    })
-    setArtists(data.artists.items)
-}
 
+        setToken(token)
 
-  const renderArtists = () => {
-    return artists.map(artist => (
-      <div key={artist.id}>
-          {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> :<div>No Image</div>}
-          {artist.name}
-      </div>
+    }, [])
 
+    const logout = () => {
+        setToken("")
+        window.localStorage.removeItem("token")
+    }
 
-    ))
+    const searchArtists = async (e) => {
+        e.preventDefault()
+        const {data} = await axios.get("https://api.spotify.com/v1/search", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            params: {
+                q: searchKey,
+                type: "artist"
+            }
+        })
 
+        setArtists(data.artists.items)
+    }
 
-  }
+    const renderArtists = () => {
+        return artists.map(artist => (
+            <div key={artist.id}>
+                {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+                {artist.name}
+            </div>
+        ))
+    }
 
-  return (
-    <div className="App">
+    return (
+        <div className="App">
             <header className="App-header">
                 <h1>Spotify Vote</h1>
                 {!token ?
@@ -81,7 +86,7 @@ const logout = () => {
 
             </header>
         </div>
-  );
+    );
 }
 
 export default App;
